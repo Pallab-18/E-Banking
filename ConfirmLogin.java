108c62fc4d9542e9118d9192818ccebebe2defb7
hrx0hrithik <hrithikgupta52@gmail.com>
2023-02-14 22:25:56 +0530

    after login updated

--- ConfirmLogin.java
+++ ConfirmLogin.java
@@ -18,14 +18,27 @@ public class ConfirmLogin extends HttpServlet
         {
             Class.forName("oracle.jdbc.driver.OracleDriver");
             //registering type4 driver for oracle 
-            Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","SYSTEM","cha2023");
+            Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","system","Hrithik");
             Statement stmt=con.createStatement();
-            String s="select * from app_user where email='"+login_email+"' and password='"+login_password+"' ";
+            String s="select * from app_user where email='"+login_email+"'and password='"+login_password+"' ";
             ResultSet rs=stmt.executeQuery(s);
             //pw.println(s);
             if(rs.next())
             {
              pw.println("Login Successful");
+             pw.println("<!DOCTYPE html>\n" +
+"<html lang=\"en\">\n" +
+"<head>\n" +
+"    <meta charset=\"UTF-8\">\n" +
+"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+"    <meta http-equiv=\"refresh\" content=\"3; URL=after_login.html\" />\n" +
+"    <title>redirect</title>\n" +
+"</head>\n" +
+"<body>\n" +
+"    <h2> Login Successful Wait for 3sec</h2>\n" +
+"</body>\n" +
+"</html>");
             }
             else
             {
