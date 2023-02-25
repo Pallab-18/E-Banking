d6b4bb86507a8601407f7541be5f21ba4110818a
hrx0hrithik <hrithikgupta52@gmail.com>
2023-02-25 23:50:20 +0530

    Transaction Successfull

--- SaveTran1.java
+++ SaveTran1.java
@@ -4,15 +4,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.*;//for GenericServlet
 import javax.servlet.http.*;//for HttpServlet
 
-public class SaveTran extends HttpServlet {
+public class SaveTran1 extends HttpServlet {
     public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
         res.setContentType("text/html");
         PrintWriter pw = res.getWriter();
+        java.util.Date date = new java.util.Date();
 
         String amount = req.getParameter("amount");
         try {
@@ -21,31 +20,40 @@ public class SaveTran extends HttpServlet {
             String rev_acc_typ = (String) ses.getAttribute("receiver_account_type");
             String rev_name = (String) ses.getAttribute("receiver_name");
             String rev_bank = (String) ses.getAttribute("receiver_bank");
-            String rev_ifsc = (String) ses.getAttribute("receiver_ifsc ");
+            String rev_ifsc = (String) ses.getAttribute("receiver_ifsc");
             String pay_mode = (String) ses.getAttribute("transaction_type");
             String user_email = (String) ses.getAttribute("user_email");
-
+            
             Class.forName("oracle.jdbc.driver.OracleDriver");
             // registering type4 driver for oracle
             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","HRITHIK","Hrithik");
             Statement stmt = con.createStatement();
-            String q1 = "select * from APP_USER where EMAIL='"+user_email+"'";
-            ResultSet rs = stmt.executeQuery(q1);
+            Statement stmt1 = con.createStatement();
+            Statement stmt2 = con.createStatement();
 
-            if (rs.next()) {
-//                pw.println(q1);
-                String uid = rs.getString(1);
-//                pw.println(uid);
-                String q2 = "insert into tran_table values(TRAN_ID.nextval,'"+uid+"','"+pay_mode+"','"+rev_acc+"','" +rev_acc_typ+"','"+rev_name+"','"+rev_bank+"','"+rev_ifsc+"',null,null)";
-                int rowsInserted = stmt.executeUpdate(q2);
-//                pw.println(q1);
+           
+            String q1 = "select * from APP_USER where EMAIL='"+user_email+"'";
+            ResultSet rs3 = stmt.executeQuery(q1);
+            
+             
+
+            if( rs3.next()) {
+                String uid = rs3.getString(1);
+                
+                ResultSet rs2 = stmt1.executeQuery("SELECT TRAN_ID.NEXTVAL FROM dual");
+                    if(rs2.next()){
+                  int tid= rs2.getInt(1);               
+
+                String q2 = "insert into tran_table values('"+tid+"','"+uid+"','"+pay_mode+"','"+rev_acc+"','" +rev_acc_typ+"','"+rev_name+"','"+rev_bank+"','"+rev_ifsc+"',null,null,'"+amount+"')";
+                int rowsInserted = stmt2.executeUpdate(q2);
 
                 if (rowsInserted > 0) {
-                    pw.println("Transaction Successful");
-//                    pw.println(uid);
+                    pw.println("Transaction successful");
+
                 } else {
                     pw.println("Transaction Unsuccessful");
                 }
+                    }
             } else {
                 pw.println("User not found");
             }
