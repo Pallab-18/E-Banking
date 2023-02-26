1b4e0ee50c743695a4347b0f31a66278bcce19e9
Subhrajit Mukherjee <subhrajitmukh1234@gmail.com>
2023-02-26 16:13:11 +0530

    NEW

--- ConfirmUPI.java
+++ ConfirmUPI.java
@@ -7,47 +7,100 @@ public class ConfirmUPI extends HttpServlet {
     {
         res.setContentType("text/html");
         PrintWriter pw=res.getWriter();
-        String Yvpa = req.getParameter("yvpa");
-        String Svpa = req.getParameter("svpa");
-        String Nick = req.getParameter("nick");
+        String yvpa = req.getParameter("yvpa");
+        String svpa = req.getParameter("svpa");
+        String nick = req.getParameter("nick");
+        String email=req.getParameter("email");
+        String transaction_type = req.getParameter("mode_of_pay");
         
          try{
             HttpSession ses=req.getSession();
-            
-            ses.setAttribute("yvpa",Yvpa);
-            ses.setAttribute("svpa",Svpa);
-            ses.setAttribute("nick",Nick);
+            ses.setAttribute("transaction_type",transaction_type);
+            ses.setAttribute("yvpa",yvpa);
+            ses.setAttribute("svpa",svpa);
+            ses.setAttribute("nick",nick);
+            ses.setAttribute("email",email);
         
             
             pw.println("<html>\n" +
-"  <head>\n" +
+"  <head> \n" +
+"    <style>\n" +
+"        form {\n" +
+"          width: 500px;\n" +
+"          margin: 0 auto;\n" +
+"          padding: 30px;\n" +
+"          border: 1px solid #ccc;\n" +
+"          border-radius: 10px;\n" +
+"        }\n" +
+"  \n" +
+"        h2 {\n" +
+"          text-align: center;\n" +
+"          margin-bottom: 30px;\n" +
+"        }\n" +
+"  \n" +
+"  \n" +
+"        .form-group {\n" +
+"          display: flex;\n" +
+"          flex-direction: column;\n" +
+"          margin-bottom: 20px;\n" +
+"        }\n" +
+"  \n" +
+"        .form-group label {\n" +
+"          font-weight: bold;\n" +
+"          margin-bottom: 10px;\n" +
+"        }\n" +
+"  \n" +
+"        .form-group input[type=\"text\"], .form-group input[type=\"number\"], .form-group input[type=\"email\"],.form-group select {\n" +
+"          padding: 10px;\n" +
+"          font-size: 16px;\n" +
+"          width: 100%;\n" +
+"          border-radius: 5px;\n" +
+"          border: 1px solid #ccc;\n" +
+"        }\n" +
+"  \n" +
+"      \n" +
+"        input[type=\"submit\"] {\n" +
+"          width: 120px;\n" +
+"          height: 40px;\n" +
+"          background-color: #4CAF50;\n" +
+"          color: #fff;\n" +
+"          border: none;\n" +
+"          border-radius: 5px;\n" +
+"          cursor: pointer;\n" +
+"          margin-top: 20px;\n" +
+"        }\n" +
+"        </style>\n" +
 "  </head>\n" +
 "  <body>\n" +
-"    <form>\n" +
+"    <form  action=\"Saveupi\" method=\"post\">\n" +
 "      <h2>Money Sending Form</h2>\n" +
 "      <div class=\"form-group\">\n" +
+"        <input name=\"mode_of_pay\" value=\"UPI Transaction\" hidden>\n" +
 "      \n" +
+"        <div class=\"form-group\">\n" +
+"            <label for=\"receiver-bank\">Email:</label>\n" +
+"            <input type=\"text\" id=\"receiver-bank\" name=\"email\" value=\""+email+"\" hideden>\n" +
+"          </div>\n" +
 "      <div class=\"form-group\">\n" +
-"        <label for=\"receiver-account-number\">Your VPA:</label>\n" +
-"        <input type=\"email\" id=\"card_number\" name=\"yvpa\" value=\""+Yvpa+"\" disabled>\n" +
+"       <label for=\"receiver-account-number\">Your VPA:</label>\n" +
+"        <input type=\"email\" id=\"card_number\" name=\"yvpa\" value=\""+yvpa+"\" disabled>\n" +
 "      </div>\n" +
 "\n" +
 "\n" +
 "      <div class=\"form-group\">\n" +
 "        <label for=\"receiver-name\">Send to VPA:</label>\n" +
-"        <input type=\"email\" id=\"receiver-name\" name=\"svpa\" value=\""+Svpa+"\"  disabled>\n" +
+"        <input type=\"email\" id=\"receiver-name\" name=\"svpa\" value=\""+svpa+"\"  disabled>\n" +
 "      </div>\n" +
 "\n" +
 "      <div class=\"form-group\">\n" +
 "        <label for=\"receiver-bank\">Nick Name:</label>\n" +
-"        <input type=\"text\" id=\"receiver-bank\" name=\"nick\" value=\""+Nick+"\" disabled>\n" +
+"        <input type=\"text\" id=\"receiver-bank\" name=\"nick\" value=\""+nick+"\" disabled>\n" +
 "      </div>\n" +
-"      \n" +
-"      <div class=\"form-group\">\n" +
-"        <label for=\"receiver-bank\">Amaount:</label>\n" +
-"        <input type=\"number\" id=\"amaount\" name=\"amaount\" required>\n" +
+"      <div class=\"form-group\"> \n" +
+"        <label for=\"receiver-bank\">Amount:</label>\n" +
+"        <input type=\"number\" id=\"amount\" name=\"amount\" required>\n" +
 "      </div>\n" +
-"      <input type=\"submit\" value=\"Confirm\">\n" +
+"      <input type=\"submit\" value=\"Submit\">\n" +
 "    </form>\n" +
 "  </body>\n" +
 "</html>");
---
+++ Saveupi.java
@@ -0,0 +1,158 @@
+import java.io.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.servlet.*;//for GenericServlet
+import javax.servlet.http.*;//for HttpServlet
+
+public class Saveupi extends HttpServlet {
+    public void doPost(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+        res.setContentType("text/html");
+        PrintWriter pw = res.getWriter();
+        java.util.Date date = new java.util.Date();
+
+        String amount = req.getParameter("amount");
+        try {
+            HttpSession ses = req.getSession();
+            String rev_name = (String) ses.getAttribute("nick");
+            String user_email = (String) ses.getAttribute("email");
+            String  yvpa = (String)ses.getAttribute("yvpa");
+            String svpa=(String)ses.getAttribute("svpa");
+            String pay_mode = (String) ses.getAttribute("transaction_type");
+            //String nick=(String)ses.getAttribute("nick");
+            
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+            // registering type4 driver for oracle
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","RICK","123456789");
+            Statement stmt = con.createStatement();
+            Statement stmt1 = con.createStatement();
+            Statement stmt2 = con.createStatement();
+
+            String q1 = "select * from APP_USER where EMAIL='"+user_email+"'";
+             //pw.println(q1);
+            ResultSet rs3 = stmt.executeQuery(q1);
+            
+            if( rs3.next()) {
+                String uid = rs3.getString(1);
+                String fname=rs3.getString(2);
+                String lname=rs3.getString(4);
+                
+                ResultSet rs2 = stmt1.executeQuery("SELECT TRAN_ID.NEXTVAL FROM dual");
+                    if(rs2.next()){
+                  int tid= rs2.getInt(1);    
+
+                
+                String q2 = "insert into tran_table values('"+tid+"','"+uid+"','"+pay_mode+"',null,null,'"+rev_name+"',null,null,'"+yvpa+"','"+svpa+"','"+amount+"')";
+                //pw.println(q2);
+                int rowsInserted = stmt2.executeUpdate(q2);
+                
+
+                if (rowsInserted > 0) {
+                    pw.println("<!DOCTYPE html>\n" +
+"<html lang=\"en\">\n" +
+"  <head>\n" +
+"    <meta charset=\"UTF-8\">\n" +
+"    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
+"    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
+"    <title>Receipt</title>\n" +
+"    <style>\n" +
+"      body {\n" +
+"        background: linear-gradient(white, #2e555e);\n" +
+"        height: 92vh;\n" +
+"      }\n" +
+"\n" +
+"      .receipt {\n" +
+"        width: 520px;\n" +
+"        margin: 50px auto;\n" +
+"        border: 1px solid #ccc;\n" +
+"        border-radius: 5px;\n" +
+"        padding: 20px;\n" +
+"        display: flex;\n" +
+"        flex-direction: column;\n" +
+"        align-items: center;\n" +
+"        background: linear-gradient(#6dd5edd1, white);\n" +
+"        box-shadow: 5px 5px 10px black;\n" +
+"      }\n" +
+"\n" +
+"      .transaction {\n" +
+"        display: flex;\n" +
+"        justify-content: space-between;\n" +
+"      }\n" +
+"\n" +
+"      .transaction h2 {\n" +
+"        margin-top: 0;\n" +
+"      }\n" +
+"\n" +
+"      .qr-code {\n" +
+"        border: 1px solid black;\n" +
+"        width: 110px;\n" +
+"        height: 110px;\n" +
+"        margin: 20px 0;\n" +
+"      }\n" +
+"\n" +
+"      .qr-code img {\n" +
+"        width: 110px;\n" +
+"        height: 110px;\n" +
+"      }\n" +
+"\n" +
+"      .sender-receiver {\n" +
+"        display: flex;\n" +
+"        justify-content: space-between;\n" +
+"        padding: 5px 20px;\n" +
+"      }\n" +
+"\n" +
+"      .sender, .receiver {\n" +
+"        flex-basis: 48%;\n" +
+"        padding: 0 20px;\n" +
+"      }\n" +
+"\n" +
+"      .sender h2, .receiver h2 {\n" +
+"        margin-top: 0;\n" +
+"      }\n" +
+"    </style>\n" +
+"  </head>\n" +
+"  <body>\n" +
+"    <div class=\"receipt\">\n" +
+"      <h2 style=\"margin-bottom: 0px;\">Transaction Details</h2>\n" +
+"      <hr color=\"black\" width=\"100%\">\n" +
+"      <div style=\"width: 100%\">\n" +
+"        <p class=\"transaction\"><span><strong>Transaction ID: </strong>"+tid+"</span>\n" +
+"          <span><strong>Date & Time: </strong>"+date.toString()+"</span></p>\n" +
+"        <p class=\"transaction\"><span><strong>Amount: </strong>&#8377 "+amount+"</span>\n" +
+"          <span><strong>Transaction Type: </strong>"+pay_mode+"</span></p>\n" +
+"      </div>\n" +
+"      <div class=\"qr-code\">\n" +
+"        <img src=\"qr-code.svg\" alt=\"QR Code\">\n" +
+"      </div>\n" +
+"      <div class=\"sender-receiver\">\n" +
+"        <div class=\"sender\">\n" +
+"          <h2>Sender Details</h2>\n" +
+"          <p><strong>Name: </strong>"+fname+" "+ lname+"</p>\n" +
+"          <p><strong>Email: </strong>"+ user_email +"</p>\n" +
+"          <p><strong>Your UPI ID: </strong>"+yvpa+"</p>\n" +
+"        </div>\n" +
+"        <div class=\"receiver\">\n" +
+"          <h2>Receiver Details</h2>\n" +
+"          <p><strong>Name: </strong>"+rev_name+"</p>\n" +
+"          <p><strong>Receiver UPI ID: </strong>"+svpa+"</p>\n" +
+"          </div>\n" +
+"          </div>\n" +
+"        </div>\n" +
+"        </body>\n" +
+"        </html>");
+
+                } else {
+                    pw.println("Transaction Unsuccessful");
+                }
+                    }
+            } else {
+                pw.println("User not found");
+            }
+            con.close();
+        } catch (ClassNotFoundException | SQLException e) {
+            pw.println(e);
+        }
+    }
+}
